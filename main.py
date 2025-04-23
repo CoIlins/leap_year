5973e3c55609b63139830379680243bafeddceea
collins <collinskinoti2006@gmail.com>
2025-04-23 10:58:49 +0300

    leap construct

---
+++ main.py
@@ -0,0 +1,23 @@
+def is_leap(year) ->bool:
+
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+             if (year % 400 == 0):
+                 return True
+             else:
+                 return False
+        else:
+              return True
+    else:
+        return False
+
+
+
+print("Please enter the year:")
+year = int(input())
+leap:bool = is_leap(year)
+
+if leap == True:
+    print("The year is a leap year")
+else:
+    print("The year is not a leap year")
